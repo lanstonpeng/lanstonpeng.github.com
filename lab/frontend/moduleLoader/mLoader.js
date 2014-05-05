39ebd33570a0d089901e9d4f9252fd30700be85d
lanstonpeng <lanstonpeng@gmail.com>
2014-05-05 09:54:34 +0800

    continue working on mLoader

--- lab/frontend/moduleLoader/mLoader.js
+++ lab/frontend/moduleLoader/mLoader.js
@@ -1,48 +1,86 @@
+/*
+ Question
+    between define and onload,is there any chance for other module to finish onload
+
+ TODO
+
+ context saving
+ 问题在在无依赖的Node调用完后如何调用父级的依赖检测callback
+ * */
 var Loader = (function(window){
     var document = window.document;
     var config = {
         modulePath:"./"
     };
     var callbackArr = [];
+    var currentLoadedNode,
+        loadedModuleDic = {};
 
-    var Node = function(moduleName,deps,parent,callback){
-        this.moduleName = moduleName;
-        this.deps = deps;
-        this.callback = callback;
-        this.parent = parent;
+    var Node = function(options){
+        this.moduleName = options.moduleName;
+        this.deps = options.deps;
+        this.loadedDeps = options.loadedDeps || [];
+        this.factory = options.factory;
+        this.returnValue = options.returnValue;
+        this.callback = options.callback;
     }
 
+    function considerRun(currentModuleName,parentNode){
+        if(!parentNode) return;
+        var idx = parentNode.deps.indexOf(currentModuleName);
+        parentNode.loadedDeps.push(loadedModuleDic[currentModuleName].returnValue);
+        parentNode.deps.splice(idx,1);
+        if(!parentNode.deps.length){
+            parentNode.returnValue = parentNode.factory.apply(window,parentNode.loadedDeps);
+        }
+    }
     function importModule(moduleName,callback){
         var script = document.createElement("script");
         script.src = config.modulePath + moduleName + ".js";
         document.body.appendChild(script);
-        //callbackArr.push(callback);
-        var node = new Node(moduleName,[],callback);
+
         script.addEventListener("load",function(){
+            console.log(moduleName ," loaded");
+            currentLoadedNode.moduleName = moduleName;
+            loadedModuleDic[moduleName] = currentLoadedNode;
+
+            var dependence = currentLoadedNode.deps;
+            for(var i = 0 ,len = dependence.length;i<len ;i++){
+                //check if exists
+                if(!loadedModuleDic[dependence[i]]){
+                    var cb = (function(currentLoadedNode){
+                                return function(moduleName){
+                                    considerRun(moduleName,currentLoadedNode);
+                                }
+                             })(currentLoadedNode);
+
+                    importModule(dependence[i],cb);
+                }
+            }
+            currentLoadedNode.callback = callback;
+            if(!currentLoadedNode.deps.length){
+                callback && callback.call(window,currentLoadedNode.moduleName);
+            }
         });
     }
     function define(deps,factory){
-        console.log("define");
-        var depModules = [];
         if(arguments.length == 2){
-            for(var i = 0 ,len = deps.length;i<len ;i++){
-            }
         }
         else{
             factory = deps;
+            deps = []
         }
-        var module = factory.apply(this,depModules);
-        var callback  = callbackArr.pop();
-        callback.call(this,module)
+
+        currentLoadedNode = new Node({
+            deps:deps,
+            factory:factory
+        });
     }
+
     var main = document.querySelector("script[data-main]");
-    var mainScript = document.createElement("script");
-    mainScript.src = main.getAttribute("data-main") + ".js";
-    document.body.appendChild(mainScript);
-    mainScript.onload = function(){
-       var node = new Node(this.src,[],null,null);
-        console.log("onload");
-    }
+    importModule(main.getAttribute("data-main"),function(){
+        considerRun("main",null);
+    });
     return {
         define:define
     }
