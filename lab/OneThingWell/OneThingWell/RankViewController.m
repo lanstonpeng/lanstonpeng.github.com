d0453d680e05ed5a51cc66b1c00869582f6c6512
lanstonpeng <lanstonpeng@gmail.com>
2014-10-07 10:29:27 +0800

    add sketch design

--- lab/OneThingWell/OneThingWell/RankViewController.m
+++ lab/OneThingWell/OneThingWell/RankViewController.m
@@ -15,17 +15,45 @@
 
 @property (strong,nonatomic)NSArray* favDataArr;
 
+@property (strong,nonatomic)UIActivityIndicatorView*  loadingView;
+
+@property (nonatomic)BOOL isDone;
 @end
+
+static UIEdgeInsets rankTableViewOriginalInset;
 
 @implementation RankViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.loadingView = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    self.loadingView.frame = CGRectMake(self.view.frame.size.width/2 - self.loadingView.frame.size.width/2, self.view.frame.size.height -self.tabBarController.tabBar.frame.size.height, 20, 20);
+    [self.view addSubview:self.loadingView];
+    self.loadingView.hidden = YES;
+    _isDone = YES;
+    [self fetchFavTopData];
+}
+
+- (void)fetchFavTopData
+{
+    if(!_isDone)return;
+    _isDone = NO;
     NSString *cql = [NSString stringWithFormat:@"select * from %@ limit 10 order by FavCount desc", @"AppTest"];
     [AVQuery doCloudQueryInBackgroundWithCQL:cql callback:^(AVCloudQueryResult *result, NSError *error) {
         self.favDataArr = result.results;
         [self.tableView reloadData];
+        [UIView animateWithDuration:0.3 animations:^{
+            self.tableView.contentInset = rankTableViewOriginalInset;
+            self.loadingView.hidden = YES;
+            [self.loadingView stopAnimating];
+        }];
+        _isDone = YES;
     }];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    rankTableViewOriginalInset = self.tableView.contentInset;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -65,6 +93,20 @@
     }
 }
 
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    if(scrollView.contentOffset.y < -rankTableViewOriginalInset.top - 100)
+    {
+        [self fetchFavTopData];
+        self.loadingView.frame = CGRectMake(self.view.frame.size.width/2 - self.loadingView.frame.size.width/2, 30, self.loadingView.frame.size.width, self.loadingView.frame.size.height);
+        [UIView animateWithDuration:0.3 animations:^{
+            UIEdgeInsets edgeInset = UIEdgeInsetsMake(rankTableViewOriginalInset.top + 40, rankTableViewOriginalInset.left, rankTableViewOriginalInset.bottom , rankTableViewOriginalInset.right);
+            scrollView.contentInset = edgeInset;
+            self.loadingView.hidden = NO;
+            [self.loadingView startAnimating];
+        }];
+    }
+}
 /*
 #pragma mark - Navigation
 
