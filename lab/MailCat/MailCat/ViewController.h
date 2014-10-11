17ca846c10f04087d83bace150f3d9f1987621d6
lanstonpeng <lanstonpeng@gmail.com>
2014-10-11 15:20:14 +0800

    add new project

---
+++ lab/MailCat/MailCat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MailCat
+//
+//  Created by Lanston Peng on 10/11/14.
+//  Copyright (c) 2014 Vtm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
