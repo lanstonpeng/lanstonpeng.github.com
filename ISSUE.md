82359a8f580ccf379bc619fdf68fbafc2a528dbc
lanstonpeng <lanstonpeng@gmail.com>
2014-10-15 18:45:35 +0800

    add issue markdown

---
+++ ISSUE.md
@@ -0,0 +1,1 @@
+- how to bind email with user
---
+++ lab/MailCat/MailCat/ResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultViewController.h
+//  MailCat
+//
+//  Created by Lanston Peng on 10/15/14.
+//  Copyright (c) 2014 Vtm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultViewController : UIViewController
+
+@end
