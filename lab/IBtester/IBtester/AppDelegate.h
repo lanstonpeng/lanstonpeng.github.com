d8fd551e7c0b2621c5eccd911c802726b4fdd17f
lanstonpeng <lanstonpeng@gmail.com>
2014-10-30 18:42:24 +0800

    add resume

---
+++ lab/IBtester/IBtester/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IBtester
+//
+//  Created by Lanston Peng on 10/29/14.
+//  Copyright (c) 2014 Vtm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lab/IBtester/IBtester/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IBtester
+//
+//  Created by Lanston Peng on 10/29/14.
+//  Copyright (c) 2014 Vtm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
